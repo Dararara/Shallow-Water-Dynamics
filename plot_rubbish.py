496653d1aa600df674ba70283b0c556b45ed9d3c
Dararara <1402982842@qq.com>
2019-08-05 14:34:26 -0500

    new

--- plot_rubbish.py
+++ plot_rubbish.py
@@ -2,25 +2,24 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-def pred_draw(input_file,output_file,samples):
-    inf = np.load(input_file)
-    of = np.load(output_file)
 
+
+
+def pred_draw(input_file = 'pred_out.npy', output_file = 'true_out.npy', samples = 10, nx = 31, ny = 31):
+    try:
+        inf = np.load(input_file)
+        of = np.load(output_file)
+    except:
+        print("can't find the file")
+        return
 #ri = np.load('D:/project/random/random_in_23333.npy')
 #ro = np.load( 'D:/project/random/random_out_23333.npy')
 
-    Lx = 30
-    Ly = 30
-    nx = 31
-    ny = 31
-    dx = Lx/(nx-1)
-    dy = Ly/(ny-1)
     xcord = np.arange(1,nx+1,1)
     ycord = np.arange(1,ny+1,1)
     X,Y = np.meshgrid(xcord,ycord)
-
-    rin = inf.reshape((inf.shape[0],nx,ny))
-    ron = of.reshape((of.shape[0],nx,ny))
+    rin = inf.reshape((-1,nx,ny))
+    ron = of.reshape((-1,nx,ny))
 
     for i in range(min(inf.shape[0],samples)):
         fig = plt.figure()
@@ -29,3 +28,5 @@ def pred_draw(input_file,output_file,samples):
         in_curve.plot_surface(X,Y, rin[i,:,:],cmap='rainbow')
         out_curve.plot_surface(X,Y, ron[i,:,:],cmap='rainbow')
         plt.show()
+
+pred_draw('pred_out.npy', 'true_out.npy')
