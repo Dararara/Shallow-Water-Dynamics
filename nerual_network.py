9333c6cc7ac276779127d4762e0f9d9d132b3a7d
Dararara <1402982842@qq.com>
2019-08-05 16:54:31 -0500

    hhh

--- nerual_network.py
+++ nerual_network.py
@@ -8,11 +8,8 @@ import re
 import numpy as np
 import tensorflow as tf
 from keras import regularizers
-
-dinput = np.load('din.npy')
-doutput = np.load('dout.npy')
-
-def cnn(din = dinput, dout = doutput, stop_patience = 3, filter_num = 64, kernel_size_1 = (3,3), activation_1 = 'tanh'):
+def cnn(din, dout, stop_patience = 3, filter_num = 64, kernel_size_1 = (3,3), activation_1 = 'tanh'):
+    
     checkpointer = callbacks.ModelCheckpoint(filepath='best_model.h5', verbose=1, save_best_only=True)
     tensorboard = callbacks.TensorBoard(write_images=True, log_dir = 'log') 
     mycall = callbacks.EarlyStopping(patience=stop_patience, monitor='loss', mode = 'min', min_delta=0.0001)
@@ -23,5 +20,3 @@ def cnn(din = dinput, dout = doutput, stop_patience = 3, filter_num = 64, kernel
     model.compile(optimizer='Adam', loss='mse', metrics=[ 'mse', 'mae'])
     history = model.fit(batch_size=100, epochs=20, callbacks=[mycall, tensorboard, checkpointer], x=din.reshape(din.shape[0], 31, 31, 1), y = dout.reshape(dout.shape[0], 961), verbose=1)
     model.save('cnn_{}_{}_{}.h5'.format(filter_num, kernel_size_1[0], kernel_size_1[1]))
-
-cnn()
