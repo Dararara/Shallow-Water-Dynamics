a7f6dc853ab7c5ac73b4495e95505429fc8cca30
Dararara <1402982842@qq.com>
2019-08-05 14:48:28 -0500

    new

--- plot_rubbish.py
+++ plot_rubbish.py
@@ -3,24 +3,24 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
-
+def predict(predict_model, input_data):
+    pass
 
 def pred_draw(input_file = 'pred_out.npy', output_file = 'true_out.npy', samples = 10, nx = 31, ny = 31):
+
+
     try:
         inf = np.load(input_file)
         of = np.load(output_file)
     except:
         print("can't find the file")
         return
-#ri = np.load('D:/project/random/random_in_23333.npy')
-#ro = np.load( 'D:/project/random/random_out_23333.npy')
 
     xcord = np.arange(1,nx+1,1)
     ycord = np.arange(1,ny+1,1)
     X,Y = np.meshgrid(xcord,ycord)
     rin = inf.reshape((-1,nx,ny))
     ron = of.reshape((-1,nx,ny))
-
     for i in range(min(inf.shape[0],samples)):
         fig = plt.figure()
         in_curve = fig.add_subplot('211',projection='3d')
