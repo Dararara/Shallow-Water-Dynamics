0c1ca0edbe2206c626ad635236bb6e05f1b80577
Dararara <1402982842@qq.com>
2019-08-06 15:25:06 -0500

    make it better

--- nerual_network.py
+++ nerual_network.py
@@ -9,26 +9,21 @@ def cnn(din , dout , test_din = None, test_dout = None, validation_split_num = 0
     #cnn with one con2D and one hidden layer
     #input is 31 * 31 * 1, output is 961
     #one callback for early stop, one callback to store the best most among all the epochs
+    #return history for plot the process
 
     checkpointer = callbacks.ModelCheckpoint(filepath='best_model_{}_{}.h5'.format(filter_num, hidden), verbose=1, save_best_only=True, period=1)
-    #tensorboard = callbacks.TensorBoard(write_images=True, log_dir = 'log') 
     mycall = callbacks.EarlyStopping(patience=stop_patience, monitor='loss', mode = 'min', min_delta=0.0001)
+    #build the model
     model = Sequential()
     model.add(Conv2D(filters = filter_num, kernel_size = kernel_size_1, activation = activation_1))
     model.add(Flatten())
     model.add(Dense(1024, activation='relu'))
     model.add(Dense(961, activation = 'linear'))
     model.compile(optimizer='Adam', loss='mse', metrics=[ 'mse', 'mae'])
+    #train the model
     history = model.fit(batch_size=100, epochs=10000, callbacks=[mycall, checkpointer], x=din.reshape(din.shape[0], 31, 31, 1), y = dout.reshape(dout.shape[0], 961), 
         verbose=2, validation_split = validation_split_num, validation_data=[test_din.reshape(test_din.shape[0], 31, 31, 1), test_dout.reshape(test_dout.shape[0], 961)])
-    #print(model.summary())
-    #model.save('cnn_{}_{}_{}.h5'.format(filter_num, kernel_size_1[0], kernel_size_1[1]))
-    
-def test_cnn(din, dout, file_name = 'best_model_8.h5'):
-    model = load_model(file_name)
-    mse, temp, mae = model.evaluate(x = din.reshape(-1, 31, 31, 1), y = dout.reshape(-1, 961))
-    return mse, mae
-
+    return history
 
 
             
