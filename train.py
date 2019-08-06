fcbd24d9826638ea2a02107ea89fab1a200c4e8b
Dararara <36438137+Dararara@users.noreply.github.com>
2019-08-06 16:40:38 -0500

    Update train.py

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ din = np.load('random_input.npy')
 dout = np.load('random_output.npy')
 #split data into training data and test data
 test_din = np.load('random_input_2.npy')
-test_dout = np.load('random_output_2,npy')
+test_dout = np.load('random_output_2.npy')
 #train cnn with different filter numbers, 8, 16 to 256
 
 cnn(din = din, dout = dout, test_din = test_din, test_dout=test_dout, filter_num = 16, stop_patience=10, hidden=4096)
